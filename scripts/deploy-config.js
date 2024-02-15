71e5aba03cae10ee7ca4ede19b31f49623482436
Thomas Dekeyser <thomas@polymerlabs.org>
2024-02-15 21:16:08 +0100

    update for univesal channel and update README

--- scripts/deploy-config.js
+++ scripts/deploy-config.js
@@ -1,15 +1,22 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const ibcConfig = require("../ibc.json");
 
 // Run script with source and destination networks as arguments
 // Example: 
 // $ node deploy-config.js optimism base
 const source = process.argv[2];
 const destination = process.argv[3];
+const universalChannel = process.argv[4];
 
 if (!source || !destination) {
-  console.error('Usage: node deploy-config.js <source_network> <destination_network>');
+  console.error('Usage: node deploy-config.js <source_network> <destination_network> <universal_channel_bool>');
+  process.exit(1);
+}
+
+if (process.argv[4] === undefined) {
+  console.error('Usage: node deploy-config.js <source_network> <destination_network> <universal_channel_bool>');
   process.exit(1);
 }
 
@@ -19,15 +26,20 @@ function updateConfig(network, address, isSource) {
   const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
   // Update the config object
-  if (isSource) {
-    config["createChannel"]["srcChain"] = network;
-    config["createChannel"]["srcAddr"] = address;
-  } else {
-    config["createChannel"]["dstChain"] = network;
-    config["createChannel"]["dstAddr"] = address;
-  }
+  if (universalChannel === "false") {
+    if (isSource) {
+      config["createChannel"]["srcChain"] = network;
+      config["createChannel"]["srcAddr"] = address;
+    } else {
+      config["createChannel"]["dstChain"] = network;
+      config["createChannel"]["dstAddr"] = address;
+    }
 
-  config["sendPacket"][`${network}`]["portAddr"] = address;
+    config["sendPacket"][`${network}`]["portAddr"] = address;    
+  } else {
+    config["sendUniversalPacket"][`${network}`]["portAddr"] = address;
+    config["sendUniversalPacket"][`${network}`]["channelId"] = ibcConfig[`${network}`]["universalChannel"];
+  }
 
   // Write the updated config back to the file
   fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
