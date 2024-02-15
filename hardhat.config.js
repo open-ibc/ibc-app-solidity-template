8f28161e3d97e61db20a9c95f21b8213924d63b1
tmsdkeys <98807841+tmsdkeys@users.noreply.github.com>
2024-02-15 18:49:16 +0100

    Update hardhat.config.js solidity compiler version

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.24',
+    version: '0.8.23',
     settings: {
       optimizer: {
         enabled: true,
