6bedaba07db2875b60f44ccffcec54ddc349c38a
Ken Miyachi <kenmiyachi@Kens-MacBook-Pro.local>
2024-02-26 17:12:37 -0700

    fixing send packet scripts

--- scripts/send-packet.js
+++ scripts/send-packet.js
@@ -4,52 +4,56 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require('hardhat');
-const path = require('path');
-const configRelativePath = process.env.CONFIG_PATH || 'config.json';
-const configPath = path.join(__dirname, '..' , configRelativePath);
+const hre = require("hardhat");
+const path = require("path");
+const configRelativePath = process.env.CONFIG_PATH || "config.json";
+const configPath = path.join(__dirname, "..", configRelativePath);
 const config = require(configPath);
 const sendConfig = config.sendPacket;
 
 async function main() {
-    const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
+  console.log(accounts);
 
-    const networkName = hre.network.name;
-    // Get the contract type from the config and get the contract
-    const contractType = config["deploy"][`${networkName}`];
+  const networkName = hre.network.name;
+  // Get the contract type from the config and get the contract
+  const contractType = config["deploy"][`${networkName}`];
 
-    const ibcAppSrc = await hre.ethers.getContractAt(
-        `${contractType}`,
-        sendConfig[`${networkName}`]["portAddr"]
-    );
+  const ibcAppSrc = await hre.ethers.getContractAt(
+    `${contractType}`,
+    sendConfig[`${networkName}`]["portAddr"]
+  );
 
-    // Do logic to prepare the packet
-    const channelId = sendConfig[`${networkName}`]["channelId"];
-    const channelIdBytes = hre.ethers.encodeBytes32String(channelId);
-    const timeoutSeconds = sendConfig[`${networkName}`]["timeout"];
-    
-    // Send the packet
-    await ibcAppSrc.connect(accounts[1]).sendPacket(
-        channelIdBytes,
-        timeoutSeconds,
-        // Define and pass optionalArgs appropriately or remove if not needed    
-        )
-    console.log("Sending packet");
+  // Do logic to prepare the packet
+  const channelId = sendConfig[`${networkName}`]["channelId"];
+  const channelIdBytes = hre.ethers.encodeBytes32String(channelId);
+  const timeoutSeconds = sendConfig[`${networkName}`]["timeout"];
 
-    // Active waiting for the packet to be received and acknowledged
-    // @dev You'll need to implement this based on the contract's logic
-    let acked = false;
-    let counter = 0;
-    do {
-        // Define an acked by interacting with the contract. This will depend on the contract's logic
-        if (!acked) {
-            console.log("ack not received. waiting...");
-            await new Promise((r) => setTimeout(r, 2000));
-            counter++;
-        } 
-    } while (!acked && counter<100);
-    
-    console.log("Packet lifecycle was concluded successfully: " + acked);
+  let counter = await ibcAppSrc.counter();
+  console.log(`Sending packet, counter before sending: ${counter}`); // Send the packet
+  await ibcAppSrc.connect(accounts[0]).sendPacket(
+    channelIdBytes,
+    timeoutSeconds,
+    { gasLimit: 3000000 }
+    // Define and pass optionalArgs appropriately or remove if not needed
+  );
+  console.log("Sending packet");
+
+  // Active waiting for the packet to be received and acknowledged
+  // @dev You'll need to implement this based on the contract's logic
+  let acked = false;
+  do {
+    // Define an acked by interacting with the contract. This will depend on the contract's logic
+    if (!acked) {
+      console.log("ack not received. waiting...");
+      await new Promise((r) => setTimeout(r, 2000));
+      counter++;
+    }
+  } while (!acked && counter < 100);
+
+  console.log("Packet lifecycle was concluded successfully: " + acked);
+  counter = await ibcAppSrc.counter();
+  console.log(`Counter after sending: ${counter}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/send-universal-packet.js
+++ scripts/send-universal-packet.js
@@ -4,58 +4,59 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require('hardhat');
-const path = require('path');
-const configRelativePath = process.env.CONFIG_PATH || 'config.json';
-const configPath = path.join(__dirname, '..' , configRelativePath);
+const hre = require("hardhat");
+const path = require("path");
+const configRelativePath = process.env.CONFIG_PATH || "config.json";
+const configPath = path.join(__dirname, "..", configRelativePath);
 const config = require(configPath);
 const sendConfig = config.sendUniversalPacket;
 
 async function main() {
-    const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
-    const networkName = hre.network.name;
-    // Get the contract type from the config and get the contract
-    const contractType = config["deploy"][`${networkName}`];
+  const networkName = hre.network.name;
+  // Get the contract type from the config and get the contract
+  const contractType = config["deploy"][`${networkName}`];
 
-    const srcPortAddr = sendConfig[`${networkName}`]["portAddr"];
+  const srcPortAddr = sendConfig[`${networkName}`]["portAddr"];
 
-    const ibcAppSrc = await hre.ethers.getContractAt(
-        `${contractType}`,
-        srcPortAddr
-    );
+  const ibcAppSrc = await hre.ethers.getContractAt(
+    `${contractType}`,
+    srcPortAddr
+  );
 
-    // Do logic to prepare the packet
-    const destPortAddr = config["createChannel"]["srcAddr"] === srcPortAddr 
-        ? config["createChannel"]["dstAddr"] 
-        : config["createChannel"]["srcAddr"];
-    const channelId = sendConfig[`${networkName}`]["channelId"];
-    const channelIdBytes = hre.ethers.encodeBytes32String(channelId);
-    const timeoutSeconds = sendConfig[`${networkName}`]["timeout"];
-    
-    // Send the packet
-    await ibcAppSrc.connect(accounts[1]).sendUniversalPacket(
-        destPortAddr,
-        channelIdBytes,
-        timeoutSeconds,
-        // Define and pass optionalArgs appropriately or remove if not needed
-    )
-    console.log("Sending packet");
+  // Do logic to prepare the packet
+  const destPortAddr =
+    config["createChannel"]["srcAddr"] === srcPortAddr
+      ? config["createChannel"]["dstAddr"]
+      : config["createChannel"]["srcAddr"];
+  const channelId = sendConfig[`${networkName}`]["channelId"];
+  const channelIdBytes = hre.ethers.encodeBytes32String(channelId);
+  const timeoutSeconds = sendConfig[`${networkName}`]["timeout"];
 
-    // Active waiting for the packet to be received and acknowledged
-    // @dev You'll need to implement this based on the contract's logic
-    let acked = false;
-    let counter = 0;
-    do {
-        // Define an acked by interacting with the contract. This will depend on the contract's logic
-        if (!acked) {
-            console.log("ack not received. waiting...");
-            await new Promise((r) => setTimeout(r, 2000));
-            counter++;
-        } 
-    } while (!acked && counter<100);
-    
-    console.log("Packet lifecycle was concluded successfully: " + acked);
+  // Send the packet
+  await ibcAppSrc.connect(accounts[0]).sendUniversalPacket(
+    destPortAddr,
+    channelIdBytes,
+    timeoutSeconds
+    // Define and pass optionalArgs appropriately or remove if not needed
+  );
+  console.log("Sending packet");
+
+  // Active waiting for the packet to be received and acknowledged
+  // @dev You'll need to implement this based on the contract's logic
+  let acked = false;
+  let counter = 0;
+  do {
+    // Define an acked by interacting with the contract. This will depend on the contract's logic
+    if (!acked) {
+      console.log("ack not received. waiting...");
+      await new Promise((r) => setTimeout(r, 2000));
+      counter++;
+    }
+  } while (!acked && counter < 100);
+
+  console.log("Packet lifecycle was concluded successfully: " + acked);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
