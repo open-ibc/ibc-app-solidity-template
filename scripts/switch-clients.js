32620070a899cfa9fd0c130f24b654c4b02185d9
Thomas Dekeyser <thomas@polymerlabs.org>
2024-03-02 11:36:14 +0100

    fix switch-client script

--- scripts/switch-clients.js
+++ scripts/switch-clients.js
@@ -9,23 +9,31 @@ function flipConfig() {
         const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
         const tempConfig = {...config};
     
-        // Update the config object
+        // Below, we'll update the config object
+
+        // Flip the proofsEnabled flag
         config["proofsEnabled"] = !config["proofsEnabled"];
 
-        const source = tempConfig["createChannel"]["srcChain"];
-        const destination = tempConfig["createChannel"]["dstChain"];
-        config["createChannel"]["srcAddr"] = config["backup"]["sendPacket"][`${source}`]["portAddr"];
-        config["createChannel"]["dstAddr"] = config["backup"]["sendPacket"][`${destination}`]["portAddr"];
+        if (config.backup !== undefined && typeof config.backup === 'object' && Object.keys(config.backup).length > 0) {
+            const source = tempConfig["createChannel"]["srcChain"];
+            const destination = tempConfig["createChannel"]["dstChain"];
+            
+            // Update the createChannel object with backup values 
+            config["createChannel"]["srcAddr"] = config["backup"]["sendPacket"][`${source}`]["portAddr"];
+            config["createChannel"]["dstAddr"] = config["backup"]["sendPacket"][`${destination}`]["portAddr"];
 
-        config["sendPacket"] = config["backup"]["sendPacket"];
-        config["sendUniversalPacket"] = config["backup"]["sendUniversalPacket"];
+            // Update the sendPacket and sendUniversalPacket object with backup values
+            config["sendPacket"] = config["backup"]["sendPacket"];
+            config["sendUniversalPacket"] = config["backup"]["sendUniversalPacket"];
 
+        }
+        
         // Write a new backup object to the config
         config["backup"] = {
             "sendPacket": tempConfig["sendPacket"],
             "sendUniversalPacket": tempConfig["sendUniversalPacket"]
         };
-    
+
         // Write the updated config back to the file
         fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
         console.log('Config updated');
